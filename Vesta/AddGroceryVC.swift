73213607b7f08ebf9c9f6eca8e5e1f0b565e1b20
herman <hermancho08@gmail.com>
2022-01-23 03:10:21 +0800

    Get Barcode API call method

--- Vesta/AddGroceryVC.swift
+++ Vesta/AddGroceryVC.swift
@@ -66,15 +66,16 @@ class AddGroceryVC:UIViewController,AVCaptureMetadataOutputObjectsDelegate{
         
         
         if metadataObjects != nil && metadataObjects != nil{
-            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+            if let object = metadataObjects.first as? AVMetadataMachineReadableCodeObject{
                 
                 
                 if (object.type == AVMetadataObject.ObjectType.ean13 || object.type == AVMetadataObject.ObjectType.upce){
                     
-                    let detectedAlert = UIAlertController(title: "Code", message:object.stringValue, preferredStyle: .alert)
+                    let detectedAlert = UIAlertController(title: "Is this the item you want to add?", message:object.stringValue, preferredStyle: .alert)
                     
-                    detectedAlert.addAction(UIAlertAction(title: "Retake", style:.default, handler: nil))
-                    detectedAlert.addAction(UIAlertAction(title: "Close", style: .default, handler:{(alert:UIAlertAction!) in self.session.stopRunning()}))
+                    detectedAlert.addAction(UIAlertAction(title: "Add", style:.default, handler: nil))
+                    detectedAlert.addAction(UIAlertAction(title: "Retake", style: .default, handler:{(alert:UIAlertAction!) in AddGroceryVC().loadView()}))
+                    
                     
                     present(detectedAlert,animated: true,completion: nil)
                     
@@ -92,6 +93,50 @@ class AddGroceryVC:UIViewController,AVCaptureMetadataOutputObjectsDelegate{
         
         
     }// end of output function
+    
+    func getReq(){
+        
+        
+        var semaphore = DispatchSemaphore (value: 0)
+
+                var request = URLRequest(url: URL(string: "https://barcode-lookup.p.rapidapi.com/v3/products?barcode=7702018866984%22")!,timeoutInterval: Double.infinity)
+                request.addValue("barcode-lookup.p.rapidapi.com", forHTTPHeaderField: "x-rapidapi-host")
+                request.addValue("8d644bd287msh96f7c05ac86b553p136a3cjsn66e1ddc83716", forHTTPHeaderField: "x-rapidapi-key")
+                request.addValue("__cflb=0H28uyvJ4CKpQyt4K4sAVoNGmQD7bdrdBPWE62PRG7P", forHTTPHeaderField: "Cookie")
+
+                request.httpMethod = "GET"
+
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                  guard let data = data else {
+                    print(String(describing: error))
+                    semaphore.signal()
+                    return
+                  }
+                  print(String(data: data, encoding: .utf8)!)
+
+                    do{
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                        print(json)
+                    }catch{ print("erroMsg") }
+
+
+
+                  semaphore.signal()
+                }
+
+                task.resume()
+                semaphore.wait()
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
         
 }
         

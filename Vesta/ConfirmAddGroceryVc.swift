707c9ef89337947da34f737babdf3adaac3cceab
herman <hermancho08@gmail.com>
2022-01-25 03:02:01 +0800

    Fix Bug and load view for confirm add grocery page

--- Vesta/ConfirmAddGroceryVc.swift
+++ Vesta/ConfirmAddGroceryVc.swift
@@ -9,31 +9,73 @@ import Foundation
 import UIKit
 
 class ConfirmAddGroceryVc:UIViewController{
+    
     var appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     @IBOutlet weak var cfmAddName: UILabel!
     
     @IBOutlet weak var cfmAddDesc: UILabel!
     
-   
+    @IBOutlet weak var noImgLbl: UILabel!
+    
     
     @IBOutlet weak var groceryImg: UIImageView!
     
     
     
     @IBAction func addButton(_ sender: Any) {
+        // add to firebase
+        
+        
+        //segue back to tablevc
+        
+        
     }
     
     
+    @IBAction func backToGrocPg(_ sender: Any) {
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        noImgLbl.isHidden = true
+        self.navigationItem.hidesBackButton = true
         cfmAddName.text = appDelegate.productName
         
         cfmAddDesc.text = appDelegate.productCat
         
-    }
-    
         
+        if appDelegate.productImg == nil{
+            noImgLbl.isHidden = false
+            
+            
+        }
+        
+         let url = URL(string:appDelegate.productImg!)
+        
+        
+        getData(from: url!) { data, response, error in
+            guard let data = data,error == nil else{return}
+            
+            DispatchQueue.main.async() {
+                [weak self] in self?.groceryImg.image = UIImage(data: data)
+            }
+        }
+        
+        
+        
+        
+}
+    
+  
+    
+    func getData(from url:URL ,completion:@escaping (Data?,URLResponse?,Error?) ->()){
+        
+        
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
     
     
   
